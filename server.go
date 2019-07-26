4172328074e909b552d34d9c20902396795ed953
Martin Gilles <martin.gilles.42@gmail.com>
2019-07-26 20:12:14 +0200

    make server working

--- server.go
+++ server.go
@@ -34,21 +34,38 @@ func (s *Server) handleGETApp(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) handlePOSTRunLua(w http.ResponseWriter, r *http.Request) {
-	logrus.Infof("runlua: %s; q=%v", r.URL, r.URL.Query())
+	sc := r.FormValue("script")
+	logrus.Infof("run-script {\n%s\n}", sc)
+
+	ctrl, err := NewController()
+	if err != nil {
+		logrus.Errorf("new-controller: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err = RunLua(sc, ctrl)
+	if err != nil {
+		logrus.Errorf("run-lua: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	http.Redirect(w, r, "/index", http.StatusFound)
 }
 
 var appHTML = `
 <html>
 <head>
-    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-    <meta http-equiv="refresh" content="120" >
-    <title>Gonzo</title>    
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />    
+    <title>EV3</title>    
 </head>
 <body>    
 	<div class="app">
 		<h1>Enter LUA script and run</h1>
+
+		<iframe class="null-frame" name="null-frame"></iframe>
 		
-		<form class="lua-form" id="lua-form" action="/runlua" method="post">            		
+		<form class="lua-form" id="lua-form" action="/runlua" method="post" target="null-frame">            		
 		<button class="lua-run-button">Run</button>
         </form>
 		<textarea class="lua-script" name="script" form="lua-form"></textarea>
@@ -60,6 +77,18 @@ var appHTML = `
 	margin: auto;
     margin-top: 100px;
     max-width: 600px;    
+}
+
+.lua-script{
+	font-family: courier;
+	width: 800px;
+	height: 400px;
+}
+
+.null-frame{
+	height: 0px;
+	width: 0px;
+	visibility: hidden;
 }
 </style>
 </html>
